5e73756ac1ce14c14c32581c58a49adc7d812ce7
Nicorh02 <colacho020200@gmail.com>
2022-09-20 19:54:50 +0100

    cambios repositorio

--- header.php
+++ header.php
@@ -5,12 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="header.css">
+    <script defer src="header.js" type="module"> </script>
     <title>Document</title>
 </head>
 <body>
     <div id="encabezado">
-        <img id="logo" src="imagenes/logo.jpg" alt="Logo">
+        <img id="logo" src="logo/logo.jpg" alt="Logo">
         <h1>Repositorio de Imagenes</h1>
+        <span id="carga">Cargar Fotos</span>
+        <article id="texto_busqueda">
+            <label for="busqueda"> Inserte palabras claves:</label>
+            <input type="text" id="busqueda">
+        </article>
     </div>
 </body>
 </html>
--- index.js
+++ index.js
@@ -1,52 +1,5 @@
+import { cargafotos } from "./libreria.js";
+
 window.onload = function(){ 
     cargafotos();
-}
-
-function cargafotos() {
-    fetch("images_list.json")
-    .then(respuesta => respuesta.json())
-    .then(contenido => {
-        let fragmento = document.createDocumentFragment();
-        document.getElementById("tarjetero").innerHTML = "";
-        contenido.forEach((foto) => {
-            const {FileName,FileSize,Height,Width,Keywords} = foto;
-            let tarjeta = document.createElement("div");
-            tarjeta.setAttribute("class", "tarjetas");
-
-            //Cargando foto
-            let imagen = document.createElement("img");
-            imagen.src = "imagenes/" + FileName;
-            imagen.tag = "Foto: " + FileName;
-            imagen.setAttribute("class", "foto");
-            tarjeta.appendChild(imagen);
-
-            //Cargando nombre de fichero
-            let fichero = document.createElement("div");
-            fichero.innerHTML="<strong>Fichero:</strong>" + FileName;
-            tarjeta.appendChild(fichero);
-
-            //Cargando tamaño
-            let tamaño = document.createElement("div");
-            tamaño.innerHTML="<strong>Tamaño: </strong>" + (FileSize/1024000).toLocaleString('es-ES') + " MB";
-            tarjeta.appendChild(tamaño);
-
-            //Cargando alto
-            let alto = document.createElement("div");
-            alto.innerHTML="<strong>Alto: </strong>" + Height + " píxeles";
-            tarjeta.appendChild(alto);
-
-            //Cargando ancho
-            let ancho = document.createElement("div");
-            ancho.innerHTML="<strong>Ancho: </strong>" + Width + " píxeles";;
-            tarjeta.appendChild(ancho);
-
-            //Cargando palabras clave
-            let clave = document.createElement("div");
-            clave.innerHTML="<strong>Claves: </strong>" + Keywords;
-            tarjeta.appendChild(clave);
-
-            fragmento.appendChild(tarjeta);
-        })    
-        document.getElementById("tarjetero").appendChild(fragmento);
-    })
 }
